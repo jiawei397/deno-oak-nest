6ff57674c1b5b1fad0a35dd13061f1743117873c
jia wei <jw397@126.com>
2021-12-22 13:39:28 +0800

    test(param): createParamDecoratorWithLowLevel

--- src/params_test.ts
+++ src/params_test.ts
@@ -1,7 +1,10 @@
 import { Context } from "../deps.ts";
 import { assertEquals, testing } from "../test_deps.ts";
 import { Controller, Get } from "./decorators/controller.ts";
-import { createParamDecorator } from "./params.ts";
+import {
+  createParamDecorator,
+  createParamDecoratorWithLowLevel,
+} from "./params.ts";
 import { Router } from "./router.ts";
 
 Deno.test("createParamDecorator", async () => {
@@ -17,6 +20,41 @@ Deno.test("createParamDecorator", async () => {
     @Get("a")
     a(@Body() body: number) {
       callStack.push(body);
+      callStack.push(3);
+      return aResult;
+    }
+  }
+
+  const router = new Router();
+  await router.add(A);
+  const ctx = testing.createMockContext({
+    path: "/user/a",
+    method: "GET",
+  });
+
+  const mw = router.routes();
+  const next = testing.createMockNext();
+
+  await mw(ctx, next);
+  assertEquals(ctx.response.body, aResult);
+  assertEquals(callStack, [1, 2, 3]);
+});
+
+Deno.test("createParamDecoratorWithLowLevel", async () => {
+  const callStack: number[] = [];
+  const aResult = "body";
+
+  const Headers = () =>
+    createParamDecoratorWithLowLevel((_ctx: Context) => {
+      callStack.push(1);
+      return 2;
+    });
+
+  @Controller("user")
+  class A {
+    @Get("a")
+    a(@Headers() headers: number) {
+      callStack.push(headers);
       callStack.push(3);
       return aResult;
     }
