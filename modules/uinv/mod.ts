75fef461800d0226fe8060f4c2f3d562f557ac70
jw397 <jw397@126.com>
2022-08-30 12:54:53 +0800

    feat(uinv): export ajax type

--- modules/uinv/mod.ts
+++ modules/uinv/mod.ts
@@ -7,3 +7,4 @@ export * from "./src/decorators/mod.ts";
 export * from "./src/types.ts";
 
 export { Ajax, md5, nanoid } from "./deps.ts";
+export type { AjaxConfig, AjaxData, ICacheStore, Method } from "./deps.ts";
